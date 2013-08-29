39b32a1e4aa219255c2e5c85a649b619079f894e
Christian Mayer <thefox21at@gmail.com>
2013-08-29 18:51:20 +0200

    Base.

--- export.php
+++ export.php
@@ -10,6 +10,7 @@ declare(ticks = 1);
 require 'vendor/autoload.php';
 
 use \Symfony\Component\Yaml\Yaml;
+use \Symfony\Component\Yaml\Dumper;
 
 $exit = 0;
 if(function_exists('pcntl_signal')){
@@ -47,25 +48,49 @@ $postsPath = $paramters['directory'];
 
 #var_export($postsPath);exit();
 
-if(!file_exists($postsPath)){
-	mkdir($postsPath);
-}
+if(!file_exists($postsPath)){ mkdir($postsPath); }
 
+#$postsPath .= '/backup_'.date('Y-m-d_H-i-s');
+#if(!file_exists($postsPath)){ mkdir($postsPath); }
+
+$dumper = new Dumper();
 
 $client = new Tumblr\API\Client($paramters['tumblr']['consumer_key'], $paramters['tumblr']['consumer_secret'], $paramters['tumblr']['token'], $paramters['tumblr']['token_secret']);
 
 foreach($client->getUserInfo()->user->blogs as $blog){
 	$blogName = $blog->name;
 	print "blog: ".$blogName."\n";
+	
 	$blogDirPath = $postsPath.'/'.$blogName;
+	$blogPostsDirPath = $blogDirPath.'/posts';
 	if(!file_exists($blogDirPath)){
 		mkdir($blogDirPath);
-		mkdir($blogDirPath.'/posts');
+		mkdir($blogPostsDirPath);
 	}
 	
-	var_export($client->getBlogInfo($blogName));
+	$blogInfo = $client->getBlogInfo($blogName);
 	
-	break;
+	file_put_contents($blogDirPath.'/info.yml', $dumper->dump((array)$blogInfo->blog));
+	
+	for($postN = 0; $postN <= $blogInfo->blog->posts; $postN += 20){
+		print "\t posts: ".$postN." to ".($postN + 20)."\n";
+		
+		foreach($client->getBlogPosts($blogName, array('limit' => 20, 'offset' => $postN))->posts as $post){
+			print "\t\t post: ".$post->id.", ".$post->type." \n";
+			
+			$postDate = new DateTime($post->date);
+			
+			file_put_contents($blogPostsDirPath.'/post_'.$post->id.'_'.$postDate->format('Y-m-d_H-i-s').'_'.$post->type.'.yml', $dumper->dump((array)$post));
+			
+			#break;
+			if($exit) break;
+		}
+		
+		#break;
+		if($exit) break;
+	}
+	
+	#break;
 }
 
 
